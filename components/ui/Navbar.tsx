5e872c38df6552395273d379ffeb03b86eecb0fb
soufian200 <soufianxlm@gmail.com>
2021-11-15 18:41:27 +0100

    update Navbar: adding components when user is authenticated

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,5 +1,8 @@
 import { FC } from "react";
+import { AiOutlineQuestionCircle, AiOutlineSetting } from "react-icons/ai";
+import Center from "../common/Center";
 import Container from "../common/Contianer";
+import Action from "./Action";
 import Logo from "./Logo";
 
 interface NavbarProps {
@@ -21,17 +24,30 @@ const Navbar: FC<NavbarProps> = ({ styles }) => {
     const LINKS: string[] = ["home", "services", "prices", "about us", "contact us"];
 
     return <div className={`shadow-md bg-white p-4 fixed z-50 w-full left-0 top-0 h-16 ${styles}`} >
-        <Container>
-            <div className={`flex justify-between`}>
-                <Logo />
 
-                <ul className={`flex items-center`}>
-                    {
-                        LINKS.map((link, index) => <Li key={index} label={link} />)
-                    }
-                </ul>
-            </div>
-        </Container>
+        <div className={`flex justify-between`}>
+            <Logo />
+
+            <ul className={`flex items-center`}>
+                {
+                    LINKS.map((link, index) => <Li key={index} label={link} />)
+                }
+            </ul>
+
+            {/* If user authenticated */}
+            {/* <div className={`flex`}>
+                    <Action title='Help' Icon={<AiOutlineQuestionCircle size={25} />} />
+                    <Action title='Settings' Icon={<AiOutlineSetting size={25} />} />
+                    <div className={`ml-4 hover:bg-gray-100 cursor-pointer rounded-full pr-3`}>
+                        <Center>
+                            <div className={`h-10 w-10 bg-blue-200 rounded-full`}></div>
+                            <h2 className={`capitalize font-bold ml-2`}>soufian lamin</h2>
+                        </Center>
+                    </div>
+                </div> */}
+
+        </div>
+
     </div>
 }
 
