16a328dbb7bb5c41f4e20f77399c16e1f1bb171a
soufian200 <soufianxlm@gmail.com>
2021-12-08 19:38:14 +0100

    update login & signup UI

--- components/ui/BluredBg.tsx
+++ components/ui/BluredBg.tsx
@@ -7,10 +7,10 @@ interface BluredBgProps {
 const BluredBg: FC<BluredBgProps> = ({ children }) => {
     return <div className={`w-screen h-screen overflow-hidden`}>
         {/* <Image className={`w-full h-full filter blur-xl`} src={bg} /> */}
-        <div className={`w-full h-full filter blur bg-gradient-to-tr to-yellow-100  from-red-100`}></div>
+        <div className={`w-full h-full  bg-gray-100`}></div>
         <Center styles={`w-screen h-screen absolute top-0 left-0`}>
             <div className={`flex flex-col`}>
-                <div className={`p-10 bg-white  shadow-lg `}>
+                <div className={`p-10 bg-white  shadow `}>
                     {children}
                 </div>
             </div>
